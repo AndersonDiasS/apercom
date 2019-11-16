755214aa6d346e2ff1592343f4b39db123d639f9
Anderson Dias de Siqueira <andersondiassiqueira33@outlook.com>
2019-11-16 11:55:14 -0300

    cc

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -307,7 +307,20 @@
       pagination: {
         el: '.swiper-pagination',
         clickable: true,
-      },
+      }, 
+      breakpoints: {
+        // when window width is >= 320px
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 20
+        },
+        // when window width is >= 480px
+        480: {
+          slidesPerView: 2,
+          spaceBetween: 30
+        },
+ 
+      }
     
     })
   });
